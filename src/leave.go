8d3b25b74cbd7b1e5ce42302b077a1ad24a57972
Callum Stott <oetzi101@gmail.com>
2013-08-04 17:38:25 -0700

    Parse args (oh so simply)

--- src/leave.go
+++ src/leave.go
@@ -1,5 +1,20 @@
-package leave
+package main
+
+import (
+    "fmt"
+    "os"
+)
 
 func main() {
+    command, time := parseArgs(os.Args);
 
+    if command == "in" {
+        fmt.Println("Leaving in", time, "minutes...")
+    } else {
+        fmt.Println("Leaving at", time, "...")
+    }
+}
+
+func parseArgs(args []string) (string, string) {
+    return args[1], args[2]
 }
